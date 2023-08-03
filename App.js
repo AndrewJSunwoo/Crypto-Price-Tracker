30e583f00a19769aaf046b029901281ac822bef4
Wu-89 <77216963+Wu-89@users.noreply.github.com>
2023-08-03 14:44:21 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation";
+import WatchlistProvider from "./src/Contexts/WatchlistContext";
+import { RecoilRoot } from "recoil";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+
+export default function App() {
+  return (
+    <NavigationContainer
+      theme={{
+        colors: {
+          background: "#121212",
+        },
+      }}
+    >
+      <RecoilRoot>
+        <WatchlistProvider>
+          <View style={styles.container}>
+            <Navigation />
+            <StatusBar style="light" />
+          </View>
+        </WatchlistProvider>
+      </RecoilRoot>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212",
+    paddingTop: 50,
+  },
+});
